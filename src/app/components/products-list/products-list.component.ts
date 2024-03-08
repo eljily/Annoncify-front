40d5d6faad09885d6bb942bc2a76fa23a0615538
eljily <eljilysidibrahim@gmail.com>
2024-03-08 23:07:56 +0000

    added project structure

---
+++ src/app/components/products-list/products-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './products-list.component.html',
+  styleUrl: './products-list.component.scss'
+})
+export class ProductsListComponent {
+
+}
