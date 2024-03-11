ad05a6253927a60d1bd66840a41dd95904db0a8b
eljily <eljilysidibrahim@gmail.com>
2024-03-11 23:09:55 +0000

    newer

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  private api = "http://localhost:8081/api";
+
+  constructor(private http: HttpClient) { }
+
+  public getProducts() :Observable<any> {
+    return this.http.get(`${this.api}/products`);
+  }
 }
