f256b4d4443a3c3cc431283912e13d645511adbe
eljily <eljilysidibrahim@gmail.com>
2024-03-11 23:37:57 +0000

    get all products from backend

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { DataViewModule } from 'primeng/dataview';
-import { Product } from '../../shared/model/Product';
-import { Image } from '../../shared/model/Image';
+import { PaginatorModule } from 'primeng/paginator';
+import { Product } from '../../model/Product';
+import { Image } from '../../model/Image';
 import { NgFor } from '@angular/common';
-import { NgxPaginationModule } from 'ngx-pagination';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-products-list',
   standalone: true,
-  imports: [ButtonModule,DataViewModule,NgFor,NgxPaginationModule],
+  imports: [ButtonModule, DataViewModule, PaginatorModule,NgFor],
   templateUrl: './products-list.component.html',
-  styleUrl: './products-list.component.scss'
+  styleUrls: ['./products-list.component.scss']
 })
 export class ProductsListComponent implements OnInit {
+
+  constructor(private productService: ProductsService) {}
+  
   products: Product[] = [];
+  pagedProducts: Product[] = [];
+  currentPage = 1; // Use a separate property for the current page
+  rows = 14;
+  totalProducts = 0; 
 
   ngOnInit() {
     // Assuming you have a method to fetch products from a service or API
@@ -28,7 +36,7 @@ export class ProductsListComponent implements OnInit {
       id: 1,
       name: 'Iphone 15 Pro Max',
       price: 100,
-      description: 'This Iphone is one ofthe best iphone in the world it used to be and it will always be the best',
+      description: 'This Iphone is one of the best iPhones in the world. It used to be and it will always be the best.',
       createDate: new Date(),
       updateDate: new Date(),
       images: [
@@ -40,7 +48,31 @@ export class ProductsListComponent implements OnInit {
       }
     };
 
-    this.products = Array(70).fill(sampleProduct);
+    // this.products = Array(70).fill(sampleProduct);
+    this.productService.getProducts().subscribe(
+      (response: any) => {
+        // Assuming your API response structure
+        this.products = response.data;
+        this.totalProducts = response.meta.total;
+        this.updatePagedProducts();
+        console.log(this.products);
+      },
+      error => {
+        console.error('Error fetching products:', error);
+      }
+    );
+    this.updatePagedProducts();
     console.log(this.products);
+  }
+
+  updatePagedProducts() {
+    const startIndex = (this.currentPage - 1) * this.rows;
+    const endIndex = startIndex + this.rows;
+    this.pagedProducts = this.products.slice(startIndex, endIndex);
+  }
+
+  onPageChange(event: any) {
+    this.currentPage = event.page + 1; // Paginator starts from 0, adjust to 1-based index
+    this.updatePagedProducts();
   }
 }
