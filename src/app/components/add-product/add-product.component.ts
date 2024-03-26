fb9ede1af594509e1b3f9c1e8f096291aa5576e1
eljily <eljilysidibrahim@gmail.com>
2024-03-26 02:18:03 +0000

    added categories

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
+import { MessageService } from 'primeng/api'; // Import MessageService
 import { FormsModule, NgModel, NgModelGroup } from '@angular/forms';
 import { FieldsetModule } from 'primeng/fieldset';
 import { FileUpload, FileUploadModule } from 'primeng/fileupload';
@@ -7,29 +8,34 @@ import { FileUpload, FileUploadModule } from 'primeng/fileupload';
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [FormsModule,FieldsetModule,FileUploadModule],
+  imports: [FormsModule, FieldsetModule, FileUploadModule],
   templateUrl: './add-product.component.html',
-  styleUrl: './add-product.component.scss'
+  styleUrl: './add-product.component.scss',
+  providers: [MessageService] // Provide MessageService
 })
 export class AddProductComponent {
 
-constructor(private productService: ProductsService){}
+  constructor(private productService: ProductsService, private messageService: MessageService) { } // Inject MessageService
 
-product: any = {};
+  product: any = {};
   images: any[] = [];
+  msgs: any[] = []; // Define msgs array to hold messages
 
   onSubmit() {
     const formData = new FormData();
     formData.append('name', this.product.name);
     formData.append('description', this.product.description);
     formData.append('price', this.product.price);
+    formData.append('category', this.product.category);
     for (let i = 0; i < this.images.length; i++) {
       formData.append('images', this.images[i]);
     }
+    console.log(this.product)
     this.productService.addProduct(formData).subscribe(
       response => {
         console.log('Product added successfully', response);
-        // Handle success
+        this.messageService.add({severity:'success', summary:'Success', detail:'Product added successfully'}); // Display success message
+        this.clearForm(); // Clear form fields
       },
       error => {
         console.error('Error adding product', error);
@@ -37,6 +43,12 @@ product: any = {};
       }
     );
   }
+  
+  clearForm() {
+    this.product = {}; // Reset product object
+    this.images = []; // Clear images array
+  }
+  
 
   onFileSelect(event: any) {
     const files: File[] = event.files;
